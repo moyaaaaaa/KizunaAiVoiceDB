457326253b22a6751d3af60c389cb09776c56027
moyaaaaaa <danmakubi@gmail.com>
2017-09-30 22:07:43 +0900

    create voice model

--- app/models/voice.rb
+++ app/models/voice.rb
@@ -1,7 +1,8 @@
 class Voice < ApplicationRecord
-  validates :filename, presence: true,
-            length: { maximum: 255 },
-            uniqueness: true
+  mount_uploader :voice_file, VoiceFileUploader
+  serialize :voice_file, JSON
+
+  validates :voice_file, presence: true, uniqueness: true
   validates :line, presence: true,
             length: { maximum: 255 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930080031) do
+ActiveRecord::Schema.define(version: 20170930125716) do
 
   create_table "voices", force: :cascade do |t|
-    t.string "filename"
+    t.string "voice_file"
     t.string "line"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["filename"], name: "index_voices_on_filename", unique: true
+    t.index ["voice_file"], name: "index_voices_on_voice_file", unique: true
   end
 
 end
