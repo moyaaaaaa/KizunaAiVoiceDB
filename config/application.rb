bcad7b4db0444e23dae4117ef0d7642d6017e14f
moyaaaaaa <danmakubi@gmail.com>
2018-04-14 14:55:31 +0900

    fix: eager_load_pathsの指定が間違っていたので修正

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module KizunaAiVoiceDB
     end
 
     config.autoload_paths += %W(#{config.root}/lib)
-    config.eager_load_paths += %W(#{config.root}/lib")
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
